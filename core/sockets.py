b19c65ad1cb4d11ccfd57bfc13026576ab20e859
dev0ps221 <seybalofficiel@gmail.com>
2023-06-10 17:59:38 +0000

    socketchatter

--- core/sockets.py
+++ core/sockets.py
@@ -4,18 +4,33 @@ class chatterSocket:
 
     def isServer(self):
         return self.isserver == True
+        
 
     def instanciate(self):
         self.socket = socket(AF_INET,SOCK_STREAM)
+        if self.isServer():
+            self.bind()
+
 
     def __init__(self,ip='',port='',isserver=0):
         self.ip       =ip
         self.port     =port
         self.isserver =isserver
 
+
+
 class chatterServer(chatterSocket):
+
+    def bind(ip=False,port:80):
+        ip = ip if ip else self.ip
+        port = port if port else self.port
+        self.socket.bind(ip,port)
+        
+
     def __init__(self,ip='',port=''):
         super().__init__(ip='',port='',1)
+
+
 
 
 class chatterClient(chatteServerSocketrSocket): 
