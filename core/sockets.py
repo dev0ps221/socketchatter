04287136e9428356ad252c9bda6d501f1cdb7a78
dev0ps221 <seybalofficiel@gmail.com>
2023-06-10 18:15:17 +0000

    sockets

--- core/sockets.py
+++ core/sockets.py
@@ -1,38 +1,49 @@
 from socket import *
 
-class chatterSocket:
+DEFAULT_SERVER_MAX_CLI = 10
 
+class chatterSocket:
+    config = {}
     def isServer(self):
-        return self.isserver == True
+        return self.config('isserver') == True
         
 
     def instanciate(self):
         self.socket = socket(AF_INET,SOCK_STREAM)
         if self.isServer():
+            self.config('maxCli',DEFAULT_SERVER_MAX_CLI)
             self.bind()
 
+    def config(self,key,val=None):
+        if key in self.config and val is None:
+            return self[key]
+        if val is not None:
+            return self.config[key] = val
 
-    def __init__(self,ip='',port='',isserver=0):
-        self.ip       =ip
-        self.port     =port
-        self.isserver =isserver
+    def __init__(self,host=gethostname(),port='',isserver=0):
+        self.config('host',host)
+        self.config('port',port)
+        self.config('isserver',isserver)
 
 
 
 class chatterServer(chatterSocket):
 
-    def bind(ip=False,port:80):
-        ip = ip if ip else self.ip
-        port = port if port else self.port
-        self.socket.bind(ip,port)
-        
+    def bind(host=False,port:80):
+        host = host if host else self.config('host')
+        port = port if port else self.config('port')
+        self.socket.bind((host,port))
+    
+    def listen(self):
+        maxCli = self.config('maxCli')
+        self.socket.listen(maxCli)
 
-    def __init__(self,ip='',port=''):
-        super().__init__(ip='',port='',1)
+    def __init__(self,host=gethostname(),port=''):
+        super().__init__(host,port,1)
 
 
 
 
 class chatterClient(chatteServerSocketrSocket): 
-    def __init__(self,ip='',port=''):
-        super().__init__(ip='',port='')
+    def __init__(self,host=gethostname(),port=''):
+        super().__init__(host,port)
